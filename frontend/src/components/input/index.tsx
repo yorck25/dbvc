3d2b9d044c5f240b1f6b855fd5fd70397ad0628f
yorck25 <100796096+yorck25@users.noreply.github.com>
2025-10-25 17:21:24 +0200

    CSS-16: * adds input component

---
+++ frontend/src/components/input/index.tsx
@@ -0,0 +1,77 @@
+import styles from "./style.module.scss";
+
+export const enum InputType {
+    TEXT = "text",
+    PASSWORD = "password",
+    EMAIL = "email",
+    NUMBER = "number",
+    CHECKBOX = "checkbox",
+    RADIO = "radio",
+}
+
+interface InputProps {
+    id: string;
+    inputType?: InputType;
+    label?: string;
+    placeholder?: string;
+    value: string;
+    handleInput: (e: Event) => void;
+    errorMessage?: string;
+    checked?: boolean;
+    disabled?: boolean;
+    name?: string;
+}
+
+export const Input = ({
+                          id,
+                          inputType = InputType.TEXT,
+                          label,
+                          placeholder,
+                          value,
+                          handleInput,
+                          errorMessage,
+                          checked,
+                          disabled = false,
+                          name,
+                      }: InputProps) => {
+    const isCheckboxOrRadio =
+        inputType === InputType.CHECKBOX || inputType === InputType.RADIO;
+
+    return (
+        <div
+            className={`${styles.input_group} ${
+                errorMessage ? styles.has_error : ""
+            } ${isCheckboxOrRadio ? styles.inline_input : ""}`}
+        >
+            {!isCheckboxOrRadio && label && (
+                <label htmlFor={id} className={styles.label}>
+                    {label}
+                </label>
+            )}
+
+            {errorMessage && (
+                <p className={styles.error_message}>{errorMessage}</p>
+            )}
+
+            <div className={styles.input_wrapper}>
+                <input
+                    type={inputType}
+                    id={id}
+                    name={name}
+                    placeholder={placeholder}
+                    value={isCheckboxOrRadio ? undefined : value}
+                    checked={isCheckboxOrRadio ? checked : undefined}
+                    onInput={handleInput}
+                    disabled={disabled}
+                    className={styles.input}
+                />
+
+                {isCheckboxOrRadio && label && (
+                    <label htmlFor={id} className={styles.inline_label}>
+                        {label}
+                    </label>
+                )}
+            </div>
+        </div>
+    );
+};
